df9e372da14224b7e0ad5418f50e51608d5f854e
Yu <k28143995@gmail.com>
2022-06-27 13:37:40 -0700

    Low energy devices discoverable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./scss/App.css";
 
 function App() {
+  let mobileNavigatorObject: any = window.navigator;
+  const [info, setInfo] = useState("");
   return (
     <div className="App">
-      <header className="App-header">gsfgsfg</header>
+      <div className="buttonContainer">
+        <button
+          onClick={() => {
+            if (mobileNavigatorObject && mobileNavigatorObject.bluetooth) {
+              mobileNavigatorObject.bluetooth
+                .requestDevice({
+                  acceptAllDevices: true,
+                  optionalServices: ["battery_service", "device_information"],
+                })
+                .then((device: any) => {
+                  console.log(device.name);
+                });
+            } else {
+              setInfo("Bluetooth not enabled on this browser");
+            }
+          }}
+        >
+          Discover devices
+        </button>
+      </div>
+      <div className="info">{info}</div>
     </div>
   );
 }
